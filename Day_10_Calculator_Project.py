47128ddf8e5832a617b1890f328986c04d7f1897
Worathep Seenuan <worathep.see@gmail.com>
2026-01-31 21:26:08 +0700

    Day_10_Project

---
+++ Day_10_Calculator_Project.py
@@ -0,0 +1,59 @@
+logo = r"""
+ _____________________
+|  _________________  |
+| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+"""
+print(logo)
+
+def add(n1, n2):
+    return n1 + n2
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1/ n2
+
+operators = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+}
+def calculator():
+    should_calculate = True
+    first_number = float(input("What's the first number?: "))
+
+    while should_calculate:
+        for symbol in operators:
+            print(symbol)
+
+        operator_option = input("Pick an operation: ")
+        second_number = float(input("What's the second number?: "))
+        answer = operators[operator_option](first_number, second_number)
+        print(f"{first_number} {operator_option} {second_number} = {answer}")
+
+        choice = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ")
+        if choice == "y":
+            first_number = answer
+
+        else:
+            should_calculate = False
+            print("\n" * 20)
+            calculator()
+            
+calculator()
